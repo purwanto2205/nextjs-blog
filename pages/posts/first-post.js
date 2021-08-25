289a2d9619a9f40909e40639da81117380449af2
Purwanto <purwanto.just1@gmail.com>
2021-08-25 15:55:15 +0700

    tutorial day 2

---
+++ pages/posts/first-post.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+const FirstPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <Image
+        src="/images/profile.jpeg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+    </Layout>
+  );
+};
+
+export default FirstPost;
